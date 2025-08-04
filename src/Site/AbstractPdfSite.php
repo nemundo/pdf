6404965dcd11c831e519ee9f5ebcd3d10bdc0c8f
Lang Urs <urs.lang@luzern.com>
2025-08-04 12:54:16 +0200

    .

--- src/Site/AbstractPdfSite.php
+++ src/Site/AbstractPdfSite.php
@@ -3,14 +3,12 @@
 namespace Nemundo\Pdf\Site;
 
 use Nemundo\Admin\Site\AbstractAdminIconSite;
-use Nemundo\Admin\Site\AbstractInactiveIconSite;
-use Nemundo\Core\Language\LanguageCode;
 use Nemundo\Web\Site\AbstractSiteTree;
 
 abstract class AbstractPdfSite extends AbstractAdminIconSite
 {
 
-    public function __construct(AbstractSiteTree $site = null)
+    public function __construct(?AbstractSiteTree $site = null)
     {
         $this->title = 'Pdf';
         $this->url = 'pdf';
